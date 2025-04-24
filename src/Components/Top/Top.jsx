80a29b41833d1569d62eb76f792e86149eb3e6ed
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-04-24 11:55:41 +0600

    first commit

---
+++ src/Components/Top/Top.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const Top = () => {
+    return (
+        <div>
+            <div>
+                <div className="navbar bg-base-100">
+                    <div className="flex-1">
+                        <a className="btn btn-ghost text-xl">daisyUI</a>
+                    </div>
+                    <div className="flex-none">
+                        <ul className="menu menu-horizontal px-1">
+                            <li>
+                                <a>Link</a>
+                            </li>
+                            <li>
+                                <details>
+                                    <summary>EN</summary>
+                                    <ul className="bg-base-100 rounded-t-none p-2">
+                                        <li>
+                                            <a>Link 1</a>
+                                        </li>
+                                        <li>
+                                            <a>Link 2</a>
+                                        </li>
+                                    </ul>
+                                </details>
+                            </li>
+                            <li>
+                                <details>
+                                    <summary>Parent</summary>
+                                    <ul className="bg-base-100 rounded-t-none p-2">
+                                        <li>
+                                            <a>Link 1</a>
+                                        </li>
+                                        <li>
+                                            <a>Link 2</a>
+                                        </li>
+                                    </ul>
+                                </details>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Top;
--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../Components/Shared/Header';
+import Top from '../Components/Top/Top';
 
 const Root = () => {
     return (
         <div>
+            <Top></Top>
             <Header></Header>
             <Outlet></Outlet>
         </div>
