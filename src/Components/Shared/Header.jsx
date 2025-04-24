f57d01454b63b07baa0c25ecaca505aaf18b15c4
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-04-24 15:55:02 +0600

    route setup

--- src/Components/Shared/Header.jsx
+++ src/Components/Shared/Header.jsx
@@ -3,11 +3,12 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div className='flex gap-4'>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/about'>About</NavLink>
-            <NavLink to='/products'>Products</NavLink>
-            <NavLink to='/blogs'>Blogs</NavLink>
+        <div className='flex gap-4 items-center px-10 font-semibold'>
+            <button className='btn bg-red-600 text-white font-semibold'>Shop by Categories </button>
+            <NavLink to='/'>Flowers</NavLink>
+            <NavLink to='/about'>All Brands</NavLink>
+            <NavLink to='/products'>Contact</NavLink>
+            <NavLink to='/blogs'>More</NavLink>
         </div>
     );
 };
